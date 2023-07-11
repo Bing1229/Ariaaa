66f866df692548c3a2f76e99bbd9449de128ff5b
limeili <78315327+Bing1229@users.noreply.github.com>
2023-07-11 09:56:32 +0200

    Update arole.js

--- commands/arole.js
+++ commands/arole.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, Events, PermissionsBitField} = require('discord.js');
+const { SlashCommandBuilder, Events, PermissionFlagsBits} = require('discord.js');
 const { welcome_db } = require('../db.js');
 
 module.exports = {
@@ -18,7 +18,7 @@ module.exports = {
     .addSubcommand(subcommand =>
       subcommand.setName("welcome")
         .setDescription('set welcome webhook')
-        .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .addRoleOption(option =>
           option.setName("target")
             .setDescription("Targer role")
